b3016cee3705d12be0d5c5c6c57b48a1a4ca63e3
Asura ! <ssovanseth@gmail.com>
2025-07-18 16:16:16 +0700

    aaaaaaaaaaaaaaaaaaa

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("idk what im doing")
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+
+    if (url.pathname !== "/" && url.pathname.endsWith("/")) {
+      url.pathname = url.pathname.slice(0, -1);
+      return Response.redirect(url.toString(), 301);
+    }
+
+    return fetch(request);
+  }
+}
