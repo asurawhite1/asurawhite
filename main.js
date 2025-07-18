262593ea024270eef49e6b9b9ffc16a49fb5e452
Asura ! <ssovanseth@gmail.com>
2025-07-18 16:24:53 +0700

    aaaaaaaaaaaaaa

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-
+    const folders = ['/', '/aboutme/', '/my-secret-love/'];
+    if (folders.includes(url.pathname)) {
+      return fetch(request);
+    }
     if (url.pathname !== "/" && url.pathname.endsWith("/")) {
       url.pathname = url.pathname.slice(0, -1);
       return Response.redirect(url.toString(), 301);
     }
 
     return fetch(request);
-  }
-}
+  },
+};
